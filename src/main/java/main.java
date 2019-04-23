865181f67a92c4f0c679045682c53590acdcc59b
Michael Pope <popem3@ad.unlv.edu>
2019-04-22 19:13:46 -0700

    Initial Commit

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+import drawing.Drawing;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        Drawing draw = new Drawing();
+        draw.main();
+    }
+}
